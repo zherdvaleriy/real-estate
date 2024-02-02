30a70e86bd2be828c3d47ecf505b1bfae96ee876
Valerii Zherdetskyi <zherdvaleriy@gmail.com>
2024-02-02 12:55:02 +0100

    added prisma configuration and connected to db

--- server/index.js
+++ server/index.js
@@ -15,10 +15,10 @@ app.use(cookieParser())
 const port = process.env.PORT || 7000
 const db = process.env.DB_URI
 
-mongoose.connect(db)
-mongoose.connection
-         .on('error', console.error)
-         .on('open', () => console.log('DB is successfully connected'))
+// mongoose.connect(db)
+// mongoose.connection
+//          .on('error', console.error)
+//          .on('open', () => console.log('DB is successfully connected'))
 
 
 app.listen(port, console.log(`Server started on port: ${port}.`) )
