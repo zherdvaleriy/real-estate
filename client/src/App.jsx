54b83ca8ec6ff9ed950086efacbde43da750d82b
Valerii Zherdetskyi <zherdvaleriy@gmail.com>
2024-02-03 23:14:59 +0100

    changes before fail

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,17 +6,22 @@ import Website from "./pages/Website"
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Layout from "./components/Layout/Layout";
 import Properties from "./pages/Properties/Properties";
+// import {QueryClient, QueryClientProvider} from 'react-query'
+// import {ReactQueryDevtools} from 'react-query/devtools'
+import { ToastContainer } from "react-toastify";
 
 
 
 
 function App() {
+  // const queryClient = new QueryClient()
 
   return (
-    <MantineProvider>
+    <MantineProvider >
 
+      {/* <QueryClientProvider client={queryClient}> */}
        <BrowserRouter>
-        <Suspense fallback={<di>Loading ...</di>}>
+        <Suspense fallback={<div>Loading ...</div>}>
           <Routes>
             <Route element={<Layout/>} >
               <Route path="/" element={<Website/>} />
@@ -25,7 +30,9 @@ function App() {
           </Routes>
         </Suspense>
        </BrowserRouter>
-
+       <ToastContainer/>
+      {/* </QueryClientProvider> */}
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
     </MantineProvider>
   );
 }
--- client/src/pages/Properties/Properties.jsx
+++ client/src/pages/Properties/Properties.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
+import SearchBar from '../../components/SearchBar/SearchBar'
+// import useProperties from '../../hooks/useProperties'
+import './Properties.css'
 
 
 
 const Properties = () => {
-  return (
-    <div className='wrapper'>Properties</div>
-  )
+//     const {data, isError, isLoading} = useProperties()
+//     console.log(data)
+//   return (
+//     <div className='wrapper'>
+//         <div className="flexColCenter paddings innerWidth properties-container">
+//           <SearchBar/>
+//         </div>
+//     </div>
+//   )
+return (
+
+<div>Properties</div>
+)
 }
 
 export default Properties
