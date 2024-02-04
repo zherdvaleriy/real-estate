54fcc7d996dfd9466f225fd9aeaff2203b6ec759
Valerii Zherdetskyi <zherdvaleriy@gmail.com>
2024-02-04 20:08:50 +0100

    Populated data from database to properties, slider

--- client/src/components/PropertyCard/PropertyCard.jsx
+++ client/src/components/PropertyCard/PropertyCard.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './PropertyCard.css'
 import { AiFillHeart } from 'react-icons/ai';
-
+import {truncate} from 'lodash'
 
 
 
@@ -17,8 +17,8 @@ const PropertyCard = ({card}) => {
         <span style={{ color: "orange" }}>$</span>
         <span>{card.price}</span>
         </span>
-        <span className="primaryText">{card.name}</span>
-        <span className="secondaryText">{card.detail}</span>
+        <span className="primaryText">{truncate(card.title, {length: 15})}</span>
+        <span className="secondaryText">{truncate(card.description, {length: 80})}</span>
     </div>
 
   )
